65245e9f3a10b877de344cc4935325f07ffd6d4d
surekhkandhare <surekha.kandhare@gmail.com>
2018-04-23 15:18:30 +0530

    by surekha

--- KapuWebsite/src/app/core/login/login.component.ts
+++ KapuWebsite/src/app/core/login/login.component.ts
@@ -4,8 +4,8 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  templateUrl: './login.component.html'
+
 })
 export class LoginComponent implements OnInit {
 
